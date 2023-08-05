4ad4228a04f8d684d99bc809dbffdabfc3040bea
Robert Cabrera <cabrera.rch@gmail.com>
2023-08-05 16:48:12 +0200

    feat: create constants file and use it in App new states

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+
 import {
   StyleSheet,
   Text,
@@ -7,12 +9,18 @@ import {
   SafeAreaView,
   Platform,
 } from "react-native";
+import { SECONDS_IN_MINUTE, TIME_TYPE } from "./src/constants";
 
 export default function App() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25 * SECONDS_IN_MINUTE);
+  const [currentTime, setCurrentTime] = useState<TIME_TYPE>("POMO");
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={{ paddingTop: Platform.OS === "android" ? 30 : 0 }}>
         <Text style={styles.text}>Pomodoro</Text>
+        <Text style={styles.text}>{currentTime}</Text>
         <Button title="My button" />
         <StatusBar style="auto" />
       </View>
---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const COLORS = ["#F7DC6F", "#A2D9CE", "#D7BDE2"];
+export const SECONDS_IN_MINUTE = 60;
+export type TIME_TYPE = "POMO" | "SHORT" | "BREAK";
