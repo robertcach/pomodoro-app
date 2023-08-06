9244baf72620450d3eb94c1be6d222b7129be85a
Robert Cabrera <cabrera.rch@gmail.com>
2023-08-06 18:33:18 +0200

    feat: format time to show timer as minutes and seconds

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,13 +1,20 @@
 import { Text, View, StyleSheet } from "react-native";
+import { SECONDS_IN_MINUTE } from "../constants";
 
 interface TimerProps {
   time: number;
 }
 
 export default function Timer({ time }: TimerProps) {
+  const formattedTime = `${Math.floor(time / SECONDS_IN_MINUTE)
+    .toString()
+    .padStart(2, "0")}:${(time % SECONDS_IN_MINUTE)
+    .toString()
+    .padStart(2, "0")}`;
+
   return (
     <View style={styles.container}>
-      <Text style={styles.time}>{time}</Text>
+      <Text style={styles.time}>{formattedTime}</Text>
     </View>
   );
 }
